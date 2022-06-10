7a42e4b6ead9f29bf44363606b44babf60c293c7
Eric Kapalka <e.bkapalka@gmail.com>
2022-06-10 10:34:20 -0600

    Attempting to get Workflow working

---
+++ index.js
@@ -0,0 +1,18 @@
+require("isomorphic-unfetch");
+const { promises: fs } = require("fs");
+const path = require("path");
+
+
+async function main() {
+    const readmeTemplate = (
+        await fs.readFile(path.join(process.cwd(), "./README.template.md"))
+    ).toString("utf-8");
+
+
+    /*const readme = readmeTemplate
+        .replace("{office_quote}", office_quote.data.content)*/
+    const readme = readmeTemplate
+    await fs.writeFile("README.md", readme);
+}
+
+main();
